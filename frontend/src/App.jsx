2b1d572c602990e1098f4c715ed2b8a8c59239b3
Vinayan C L <vinaychristo.vc@gmail.com>
2025-07-08 22:39:53 +0530

    Bookevent working on

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import {Routes, Route} from "react-router-dom"
 import Menubar from './components/Menubar.jsx'
-import './App.css'
 import Home from './components/Home';
 import Upcoming_Events from './components/Upcoming_Events.jsx'
 import Bookevent from './components/Bookevent.jsx'
